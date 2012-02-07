6ceeebce8926d7981a855f83e9f5baab16f2fff7
Ben Kurtovic <ben.kurtovic@verizon.net>
2012-02-06 19:05:25 -0500

    Minor code cleanup.

--- js/tweets.js
+++ js/tweets.js
@@ -1,5 +1,25 @@
-function show_tweets(tweets) {
-    var element = document.getElementById("tweets");
+function load_tweets() {
+    var element = document.getElementById("twitter");
+    if (element) {
+        var callback = "like_a_boss";
+        var username = "the_earwig";
+        var count = 4;
+        var url = "https://twitter.com/statuses/user_timeline/" + username + ".json?count=" + count + "&callback=" + callback;
+
+        var script = document.createElement("script");
+        var head = document.getElementsByTagName("head")[0];
+
+        window[callback] = function(tweets) {
+            head.removeChild(script);
+            show_tweets(tweets, element);
+        };
+
+        script.src = url;
+        head.appendChild(script);
+    }
+}
+
+function show_tweets(tweets, element) {
     for (t in tweets) {
         var tweet = tweets[t];
         var t = '<li class="post">' + fmt_text(tweet["text"]) + "<br />";
